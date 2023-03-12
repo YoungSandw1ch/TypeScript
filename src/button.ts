2b22a107878206e229e3e4fffc3ad5707d43704f
Sergei Mikhailenko <sersus1991@gmail.com>
2023-03-12 10:41:04 +0200

    create ts template

--- src/button.ts
+++
@@ -1,8 +0,0 @@
-const button = document.querySelector('#btnId')!;
-const onBtnClick = () => {
-  console.log("you click on button")
-};
-
-// console.log(button);
-
-button.addEventListener('click', onBtnClick);
